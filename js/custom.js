babfa97adc3872f2e99567bddbe818d034d57a8c
camsolutions <42949551+camsolutions@users.noreply.github.com>
2019-10-15 16:32:14 -0400

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,4 @@
+var x;
 function loader(){
 	setTimeout(function(){ 
 		document.getElementById('body').style.background ="none";
@@ -6,7 +7,24 @@ function loader(){
 		document.getElementById('body').className ="visible";
 		document.getElementById('body').className ="w3-animate-opacity";
 		if ( screen.width < 700 ) {
-			document.getElementsByClassName("row")[0].className = "mx-auto container-fluid text-white"
+			document.getElementsByClassName("row")[0].className = "mx-auto container-fluid text-white";
+
+			 x = document.getElementsByClassName("display-1");
+			 y = document.getElementsByClassName("content");
+			console.log(x, y);
+			x[0].className ="white-text display-2 bg-dark border-0 rounded p-2";
+			x[0].className ="black-text display-2 bg-light border-0 rounded p-2";
+			x[0].className ="white-text display-2 bg-dark border-0 rounded p-2";
+			
+			y[0].children[0].className="display-3";
+			y[0].children[1].className="h3";
+			y[1].children[0].className="display-3";
+			y[1].children[1].className="h3";
+			y[2].children[0].className="display-3";
+			y[2].children[1].className="h3";
+			y[3].children[0].className="display-3";
+			y[3].children[1].className="h3";
+
 		}
 
 	 }, 3000);
