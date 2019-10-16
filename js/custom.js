ea7de0e0458041411da2f2292407e0481323aef7
camsolutions <42949551+camsolutions@users.noreply.github.com>
2019-10-16 17:51:36 -0400

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
-var x,y,z,a;
+var x,y,z,a,nav;
+nav = document.getElementsByTagName('nav')[0];
 function loader(){
 	setTimeout(function(){ 
 		document.getElementById('body').style.background ="none";
@@ -6,12 +7,13 @@ function loader(){
 		document.getElementById('pre').className ="invisible";
 		document.getElementById('body').className ="visible";
 		document.getElementById('body').className ="w3-animate-opacity";
-		if ( screen.width < 500 ) {
+		if ( screen.width < 700 ) {
 			document.getElementsByClassName("row")[0].className = "mx-auto container-fluid text-white";
 
 			 x = document.getElementsByClassName("display-1");
 			 y = document.getElementsByClassName("content");
 			 z = document.getElementsByClassName("fas");
+			 f = document.getElementsByClassName('navbar');
 			console.log(x, y);
 			x[0].className ="white-text display-2 bg-dark border-0 rounded p-2";
 			x[0].className ="black-text display-2 bg-light border-0 rounded p-2";
@@ -20,13 +22,13 @@ function loader(){
 			y[0].className="content  mr-3 mt-5 col  border  rounded  container";
 			y[0].children[0].className="display-2";
 			y[0].children[1].className="h1";
-			y[0].className="content  mr-3 mt-5 col  border  rounded  container"
+			y[1].className="content  mr-3 mt-5 col  border  rounded  container"
 			y[1].children[0].className="display-2";
 			y[1].children[1].className="h1";
-			y[0].className="content  mr-3 mt-5 col  border  rounded  container"
+			y[2].className="content  mr-3 mt-5 col  border  rounded  container"
 			y[2].children[0].className="display-2";
 			y[2].children[1].className="h1";
-			
+			y[3].className="content  mr-3 mt-5 col  border  rounded  container"
 			y[3].children[0].className="display-2";
 			y[3].children[1].className="h1";
 
@@ -40,8 +42,41 @@ function loader(){
 			a[1].style.height ="50vh";
 			a[2].src ="img/x2/Pres3.jpg";
 			a[2].style.height ="50vh";
+
 		}
 
 	 }, 3000);
 }
+var i=0;
+function navs(){
+	if (scrollY > 50) {
+		var f =document.getElementsByClassName("nav-link black-text");
+		nav.style.background ="rgba(67,69,67,"+i+")";
+		if (f[1] != undefined ) {
+		f[0].className="nav-link white-text";
+		f[0].className="nav-link white-text";
+		f[0].className="nav-link white-text";
+		f[0].className="nav-link white-text";
+		}
+		if (i < 1) {
+		i=i + 0.2;
+		}
+	}else if (scrollY < 50){
+		var f =document.getElementsByClassName("nav-link white-text");
+		nav.style.background ="rgba(67,69,67,"+i+")";
+		if (f[1] != undefined ) {
+		f[0].className="nav-link black-text";
+		f[0].className="nav-link black-text";
+		f[0].className="nav-link black-text";
+		f[0].className="nav-link black-text";
+		}
+		if (i>0) {
+		i=i - 0.5;
+		} 
+		if (scrollY == 0) {
+			nav.style.background="rgba(255,255,255,0.8)";
+		}
+		document.getElementsByClassName("nav-link")[0].setAttribute("hover","background: white")
+	}
 
+}
