2e1b0c5136d5efe1143a40a63258550f7a72acfd
camsolutions <42949551+camsolutions@users.noreply.github.com>
2019-10-15 22:37:21 -0400

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -6,11 +6,12 @@ function loader(){
 		document.getElementById('pre').className ="invisible";
 		document.getElementById('body').className ="visible";
 		document.getElementById('body').className ="w3-animate-opacity";
-		if ( screen.width < 700 ) {
+		if ( screen.width < 500 ) {
 			document.getElementsByClassName("row")[0].className = "mx-auto container-fluid text-white";
 
 			 x = document.getElementsByClassName("display-1");
 			 y = document.getElementsByClassName("content");
+			 z = document.getElementsByClassName("fas");
 			console.log(x, y);
 			x[0].className ="white-text display-2 bg-dark border-0 rounded p-2";
 			x[0].className ="black-text display-2 bg-light border-0 rounded p-2";
@@ -19,16 +20,18 @@ function loader(){
 			y[0].className="content  mr-3 mt-5 col  border  rounded  container";
 			y[0].children[0].className="display-2";
 			y[0].children[1].className="h1";
-			y[1].className="content  mr-3 mt-5 col  border  rounded  container"
+			y[0].className="content  mr-3 mt-5 col  border  rounded  container"
 			y[1].children[0].className="display-2";
 			y[1].children[1].className="h1";
-			y[2].className="content  mr-3 mt-5 col  border  rounded  container"
+			y[0].className="content  mr-3 mt-5 col  border  rounded  container"
 			y[2].children[0].className="display-2";
 			y[2].children[1].className="h1";
-			y[3].className="content  mr-3 mt-5 col  border  rounded  container"
+			
 			y[3].children[0].className="display-2";
 			y[3].children[1].className="h1";
 
+			z[0].className="fas fa-chevron-circle-left red-text fa-2x";
+			z[1].className="fas fa-chevron-circle-right blue-text fa-2x";
 		}
 
 	 }, 3000);
